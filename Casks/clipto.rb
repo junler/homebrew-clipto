c6ad32fde738a057295300ede38a16b971715dae
sunjun <sunjun@surfin.sg>
2026-03-19 19:07:23 +0800

    v0.2.0

--- Casks/clipto.rb
+++ Casks/clipto.rb
@@ -1,11 +1,11 @@
 cask "clipto" do
-  version "0.1.0"
+  version "0.2.0"
 
   if Hardware::CPU.arm?
-    sha256 "0f479eae04aba43c4b735612833d49a4a82b123cfc2fc162aab568d38322eb62"
+    sha256 "dd58b62ff5c9f08b686878c732685121ff5f9c5fe75d655d3280621e2aad69d9"
     url "https://github.com/junler/clipto/releases/download/v#{version}/clipto_#{version}_aarch64.dmg"
   else
-    sha256 "0f479eae04aba43c4b735612833d49a4a82b123cfc2fc162aab568d38322eb62"
+    sha256 "dd58b62ff5c9f08b686878c732685121ff5f9c5fe75d655d3280621e2aad69d9"
     url "https://github.com/junler/clipto/releases/download/v#{version}/clipto_#{version}_x64.dmg"
   end
 
